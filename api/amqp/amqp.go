d933d2312474146e3bc901cc97451fde4dc8f0c0
Login Victor <batazor111@gmail.com>
2018-05-04 15:48:18 +0300

    graceful shutdown close #5

--- api/amqp/amqp.go
+++ api/amqp/amqp.go
@@ -1,31 +1,64 @@
 package amqp
 
 import (
-	"github.com/sirupsen/logrus"
 	"github.com/batazor/go-logger/utils"
+	"github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
 
 var (
 	log = logrus.New()
 
-	AMQP_API = utils.Getenv("AMQP_API", "amqp://guest:guest@localhost:5672/")
-	AMQP_NAME_QUEUE = utils.Getenv("AMQP_NAME_QUEUE", "input")
+	AMQP_API           = utils.Getenv("AMQP_API", "amqp://guest:guest@localhost:5672/")
+	AMQP_NAME_QUEUE    = utils.Getenv("AMQP_NAME_QUEUE", "input")
 	AMQP_EXCHANGE_LIST = utils.Getenv("AMQP_EXCHANGE_LIST", "demo1, demo2")
-	forever = make(chan bool)
+
+	AMQP_CONN amqp.Connection
+	AMQP_CH   amqp.Channel
+	AMQP_Q    amqp.Queue
+
+	forever      = make(chan bool)
+	gracefulStop = make(chan os.Signal)
 )
 
+func init() {
+	// Gracefully stop application
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+
+	go func() {
+		for {
+			select {
+			case <-gracefulStop:
+				exchangeList := strings.Split(AMQP_EXCHANGE_LIST, ",")
+				for _, echangeName := range exchangeList {
+					name := strings.Trim(echangeName, " ")
+					err := AMQP_CH.QueueUnbind(
+						AMQP_Q.Name,
+						name,
+						"",
+						nil,
+					)
+					failOnError(err, "Failed to unbind a queue")
+				}
+			}
+		}
+	}()
+}
+
 func Listen() {
-	conn, err := amqp.Dial(AMQP_API)
+	AMQP_CONN, err := amqp.Dial(AMQP_API)
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	defer AMQP_CONN.Close()
 
-	ch, err := conn.Channel()
+	AMQP_CH, err := AMQP_CONN.Channel()
 	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	defer AMQP_CH.Close()
 
-	q, err := ch.QueueDeclare(
+	AMQP_Q, err := AMQP_CH.QueueDeclare(
 		AMQP_NAME_QUEUE,
 		false,
 		false,
@@ -38,8 +71,8 @@ func Listen() {
 	exchangeList := strings.Split(AMQP_EXCHANGE_LIST, ",")
 	for _, echangeName := range exchangeList {
 		name := strings.Trim(echangeName, " ")
-		err = ch.QueueBind(
-			q.Name,
+		err = AMQP_CH.QueueBind(
+			AMQP_Q.Name,
 			"",
 			name,
 			false,
@@ -48,9 +81,8 @@ func Listen() {
 		failOnError(err, "Failed to bind a queue")
 	}
 
-
-	msgs, err := ch.Consume(
-		q.Name,
+	msgs, err := AMQP_CH.Consume(
+		AMQP_Q.Name,
 		"",
 		true,
 		false,
