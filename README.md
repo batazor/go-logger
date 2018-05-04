f30d86a527d62389d6571c07af8ca2c3944c5211
Login Victor <batazor111@gmail.com>
2018-05-04 12:34:49 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # go-logger
+
 Writes telemetry to TSDB
+
+### Getting start
+
+```
+docker-compose up
+```
+
+### ENV
+
+| Name         | Default value                   |
+|--------------|---------------------------------|
+| AMQP_URL     | localhost:5672                  |
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"fmt"
+)
+
+var (
+	log = logrus.New()
+)
+
+func init() {
+	// Logging =================================================================
+	// Setup the logger backend using Sirupsen/logrus and configure
+	// it to use a custom JSONFormatter. See the logrus docs for how to
+	// configure the backend at github.com/Sirupsen/logrus
+	log.Formatter = new(logrus.JSONFormatter)
+}
+
+func main() {
+	http.HandleFunc("/hello", Hello)
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
