9f3c0454003f18513d7d8751630ad8e91cb8ff0d
Login Victor <batazor111@gmail.com>
2018-08-08 14:35:42 +0300

    Refactoring AMQP

--- utils/error.go
+++ utils/error.go
@@ -6,6 +6,14 @@ var (
 	log = logrus.New()
 )
 
+func init() {
+	// Logging =================================================================
+	// Setup the logger backend using Sirupsen/logrus and configure
+	// it to use a custom JSONFormatter. See the logrus docs for how to
+	// configure the backend at github.com/Sirupsen/logrus
+	log.Formatter = new(logrus.JSONFormatter)
+}
+
 func FailOnError(err error, msg string) {
 	if err != nil {
 		log.Warn(msg, err)
